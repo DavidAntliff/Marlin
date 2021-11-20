62323e59c4597f5148332bf4ecd37c817a0ac3e3
David Antliff <david@pitchblende.co.nz>
2021-11-20 16:05:31 +1300

    PID tuning of nozzle.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -591,8 +591,8 @@
 #define PID_K1 0.95      // Smoothing factor within any PID loop
 
 #if ENABLED(PIDTEMP)
-  //#define PID_EDIT_MENU         // Add PID editing to the "Advanced Settings" menu. (~700 bytes of PROGMEM)
-  //#define PID_AUTOTUNE_MENU     // Add PID auto-tuning to the "Advanced Settings" menu. (~250 bytes of PROGMEM)
+  #define PID_EDIT_MENU         // Add PID editing to the "Advanced Settings" menu. (~700 bytes of PROGMEM)
+  #define PID_AUTOTUNE_MENU     // Add PID auto-tuning to the "Advanced Settings" menu. (~250 bytes of PROGMEM)
   //#define PID_PARAMS_PER_HOTEND // Uses separate PID parameters for each extruder (useful for mismatched extruders)
                                   // Set/get with gcode: M301 E[extruder number, 0-2]
 
@@ -603,9 +603,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  17.78
-    #define DEFAULT_Ki   1.23
-    #define DEFAULT_Kd  64.47
+    #define DEFAULT_Kp  17.99
+    #define DEFAULT_Ki   1.20
+    #define DEFAULT_Kd  67.44
   #endif
 #endif // PIDTEMP
 
